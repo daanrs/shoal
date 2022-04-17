8996e4344840d35190bb023d87707f21371794e0
daanrs <daan@spijke.rs>
2022-04-17 23:26:33 +0200

    nix: add python project

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Hanao: Haskell Natural Optimization
 Experimental library using haskell and futhark for natural optimization
-using GPU acceleration.
+using GPU acceleration. Heavy WIP!
--- flake.nix
+++ flake.nix
@@ -11,30 +11,47 @@
 
         haskellPackages = pkgs.haskellPackages;
 
-        packageName = "hanao";
+        rootDir = builtins.path { path = ./.; name = "hanao";};
+        haskellName = "hanao";
+        pythonName = "pynao";
+        futharkName = "futnao";
 
-        project = returnShellEnv:
+        pynaoProject = {
+          projectDir = rootDir;
+          overrides = pkgs.poetry2nix.overrides.withDefaults (
+            self: super: {}
+          );
+        };
+
+        pynao = pkgs.poetry2nix.mkPoetryApplication pynaoProject;
+        pynaoEnv = pkgs.poetry2nix.mkPoetryEnv pynaoProject;
+
+        hanao = returnShellEnv:
           haskellPackages.developPackage {
             inherit returnShellEnv;
-            root = ./.;
-            name = packageName;
+            root = builtins.path { path = ./.; name = haskellName; };
+            name = haskellName;
             modifier = drv:
-                pkgs.haskell.lib.addBuildTools drv
-                  (with pkgs.haskellPackages; pkgs.lib.lists.optionals returnShellEnv [
-                    # Specify your build/dev dependencies here.
-                    cabal-fmt
-                    cabal-install
-                    ghcid
-                    haskell-language-server
-                    ormolu
-                    pkgs.nixpkgs-fmt
-                  ]);
+              pkgs.haskell.lib.addBuildTools drv
+                (with pkgs.haskellPackages; pkgs.lib.lists.optionals returnShellEnv [
+                  cabal-fmt
+                  cabal-install
+                  haskell-language-server
+                  ormolu
+                  pkgs.nixpkgs-fmt
+                ]);
           };
-      in {
-        packages.${packageName} = project false;
+        hanaoEnv = hanao true;
+      in
+      {
+        packages.${haskellName} = hanao false;
 
-        defaultPackage = self.packages.${system}.${packageName};
+        defaultPackage = self.packages.${system}.${haskellName};
 
-        devShell = project true;
-      });
+        devShell = pkgs.mkShell {
+          inputsFrom = [ pynaoEnv.env hanaoEnv ];
+          buildInputs = [ pkgs.python39Packages.ipython ];
+        };
+      }
+    );
 }
