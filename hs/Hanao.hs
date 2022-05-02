91d57b77bc8a86de8c2edd4ca7e8dbf6304883cf
daanrs <daan@spijke.rs>
2022-05-02 16:36:59 +0200

    hs: add massiv, pipes, random

--- hs/Hanao.hs
+++ hs/Hanao.hs
@@ -1,3 +1,10 @@
 module Hanao where
 
+import Prelude as P
+import Data.Massiv.Array as A
+import qualified System.Random as Random
 
+gen = Random.mkStdGen 217
+
+arr :: Int -> Array DL Ix2 Double
+arr n = randomArray gen Random.split Random.random Seq (Sz2 n 2)
